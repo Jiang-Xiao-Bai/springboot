b70fc74b4603a66aa45d8ce04b275f02a2a722d2
Jiang-Xiao-Bai <1536907789@qq.com>
2020-03-04 20:58:06 +0800

    第三次提交

---
+++ src/test/java/com/ssm/springboot/MyThread.java
@@ -0,0 +1,22 @@
+package com.ssm.springboot;
+
+/**
+ * @author zhaohf
+ * @date 2020/3/4 15:42
+ */
+public class MyThread {
+
+
+    public static void main(String[] args) {
+        Thread.currentThread().setName("Main");
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0 ; i < 10 ; i++) {
+                    System.out.println(Thread.currentThread().getName()+i);
+                }
+            }
+        });
+        thread.start();
+    }
+}
