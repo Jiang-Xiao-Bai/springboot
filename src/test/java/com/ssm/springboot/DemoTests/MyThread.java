007e6ce99e0e2e3e6d177d9b89a644ceed140d09
Jiang-Xiao-Bai <1536907789@qq.com>
2020-03-12 18:19:03 +0800

    第四次提交

--- src/test/java/com/ssm/springboot/DemoTests/MyThread.java
+++ src/test/java/com/ssm/springboot/DemoTests/MyThread.java
@@ -4,19 +4,43 @@ package com.ssm.springboot.DemoTests;
  * @author zhaohf
  * @date 2020/3/4 15:42
  */
-public class MyThread {
+public class MyThread extends Thread {
 
+    public MyThread(String name) {
+        super(name);
+    }
 
-    public static void main(String[] args) {
-        Thread.currentThread().setName("Main");
-        Thread thread=new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i=0 ; i < 10 ; i++) {
-                    System.out.println(Thread.currentThread().getName()+i);
+    static int tick=20;
+
+    static MyThread thread = new MyThread("");
+
+    @Override
+    public void run() {
+        while (tick>0){
+            synchronized (thread) {
+                if(tick>0){
+                    System.out.println(this.getName()+"说：我卖出了第"+tick+"张票");
+                    tick--;
+                }else {
+                    System.out.println(this.getName()+"说：抱歉，票买完了！");
                 }
             }
-        });
-        thread.start();
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class main{
+        public static void main(String[] args) {
+            MyThread t1=new MyThread("窗口1");
+            MyThread t2=new MyThread("窗口2");
+            MyThread t3=new MyThread("窗口3");
+            t1.start();
+            t2.start();
+            t3.start();
+        }
     }
 }
